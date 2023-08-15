643bc73595d417ebac28db5b78c8a1fd5177f731
otto <ottolopes20@gmail.com>
2023-08-14 23:53:33 -0300

    perceptron logistico funcional

--- tc1/q1/perceptron/ps_sem_pca.m
+++ tc1/q1/perceptron/ps_sem_pca.m
@@ -10,7 +10,7 @@ pkg load nan
 %%% Passo 1: Carregar banco de dados %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %load ionosphere.data
-[train_data, train_labels, test_data, test_labels] = load_mnist();
+[train_data, test_data, train_labels, test_labels] = load_mnist();
 X=train_data;
 X_labels=train_labels;
 D=test_data;
@@ -21,61 +21,95 @@ D_labels=test_labels;
 N_x=size(X);  % N(1)=Numero de imagens, N(2)=dimensão X da i-esima imagem,
             % N(3)=dimensão Y da i-esima imagem
 
+N_d=size(D);  % N(1)=Numero de imagens, N(2)=dimensão X da i-esima imagem,
+            % N(3)=dimensão Y da i-esima imagem
+
 N_xlabels=size(X_labels);
 
 X = reshape(X, N_x(1),N_x(2)*N_x(3)); %Transformando de uma matriz 3D para uma 2D
-X_labels = reshape(X_labels, N_xlabels(1),N_xlabels(2)*N_xlabels(3)); %Transformando de uma matriz 3D para uma 2D
-
-X=X'; D=D'; X_labels=X_labels'; D_labels=D_labels';
+D = reshape(D, N_d(1),N_d(2)*N_d(3)); %Transformando de uma matriz 3D para uma 2D
 
 
-D=double(1./(1 + exp(-D))); %Normaliza os valores dos resultados com uma função sigmoid entre -1 e 1;
-D_labels=double(1./(1 + exp(-D_labels))); %Normaliza os valores dos resultados com uma função sigmoid entre -1 e 1;
+%X=X'; D=D'; X_labels=X_labels'; D_labels=D_labels';
+
+D=double(D);
+D_labels=double(D_labels);
+%D_labels=[ones(1,1), D_labels];
+D_labels=[ones(1,1); D_labels];
 
 X=double(X);
 X_labels=double(X_labels);
-n = size(X); %N(1) = Numero de pixels
+%X_labels=[ones(1,1), X_labels];
+X_labels=[ones(1,1); X_labels];
+
+X=double(X/255.00); %Normaliza os valores dos pixels entre 0 e 1
+D=double(D/255.00); %Normaliza os valores dos pixels entre 0 e 1
+
+X = [ones(1, N_x(2)*N_x(3)); X];
+D = [ones(1, N_d(2)*N_d(3)); D];
+
+%X=[ones(N_x(2)*N_x(3), 1); X]; %Adiciona um 1 a ao primeiro elemento para servir de bias
+%D=[ones(N_d(2)*N_d(3), 1) D]; %Adiciona um 1 a ao primeiro elemento para servir de bias
+%D=(1./(1 + exp(-D))); %Normaliza os valores dos resultados com uma função sigmoid entre -1 e 1;
+%D_labels=(1./(1 + exp(-D_labels))); %Normaliza os valores dos resultados com uma função sigmoid entre -1 e 1;
+
+
+%n = size(X); %N(1) = Numero de pixels
              %N(2) = Numero de imagens
+
+n = size(X); %N(2) = Numero de pixels
+             %N(1) = Numero de imagens
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Passo 2: Separar dados de treino/teste %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Dados de treinamento (Xtr, Dtr)
-Xtr=X;  Dtr=D;
+Xtr=X;  Dtr=X_labels;
 
 % Dados de teste (Xts, Dts)
-Xts=X_labels;  Dts=D_labels;
+Xts=D;  Dts=D_labels;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Passo 3: Estimar os parametros do classificador (pesos e limiares) %%
 %%% pelo metodo dos minimos quadrados (classificador sem camada oculta)%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-p=n(1);  % dimensao do vetor de entrada
-Ne=10;  % Numero de epocas de treinamento (numero de vezes que o conjunto de treinamento eh reapresentado)
-alfa=0.01; % Taxa de aprendizagem
+p=n(2);  % dimensao do vetor de entrada
+Ne=1000;  % Numero de epocas de treinamento (numero de vezes que o conjunto de treinamento eh reapresentado)
+alfa=0.001; % Taxa de aprendizagem
 
-W=rand(p,1);  % Inicializacao do vetor de pesos
+num_classes = 10;
+W=rand(p,num_classes);  % Inicializacao do vetor de pesos
 Ntr = n(2);
-Nts = size(Xts)(2);
+%Ntr = 10;
+Nts = size(Xts)(1);
 
+sigmoid = @(valor)1./(1 + exp(-valor));
+mapped_labels = eye(num_classes);
+
+tic();
 for t=1:Ne,
-    Itr=randperm(Ntr);
-    Xtr=Xtr(:,Itr);  % Embaralha dados a cada epoca de treinamento
-    Dtr=Dtr(Itr);
+    %Itr=randperm(Ntr);
+    %Xtr=Xtr(:,Itr);  % Embaralha dados a cada epoca de treinamento
+    %Dtr=Dtr(Itr);
     Epoca=t
     acc_erro_quad=0;  % Acumula erro quadratico por vetor em uma epoca
     for k=1:Ntr,
-        aux_sigmoid = W'*Xtr(:,k);
-        sigmoid = 1./(1 + exp(-aux_sigmoid));
-        ypred(k)=sigmoid;  % Saida predita para k-esimo vetor de entrada
-        erro(k)=Dtr(k)-ypred(k);  % erro de predicao
-        W=W+alfa*erro(k)*Xtr(:,k); % Atualizacao do vetor de pesos
+        %aux_sigmoid = W'*Xtr(:,k);
+        %sigmoid = 1./(1 + exp(-aux_sigmoid));
+        %ypred(k)=sigmoid;  % Saida predita para k-esimo vetor de entrada
+        %ypred(k)=(W'*Xtr(:,k));  % Saida predita para k-esimo vetor de entrada
+        activations(k, :) = Xtr(k,:) * W;  % Saida predita para k-esimo vetor de entrada
+        normalized_activations(k, :) = sigmoid(activations(k, :));
+        #[maior_probabilidade label_maior_probabilidade] = max(normalized_activations(k, :));
+        erro(k, :) =  mapped_labels(Dtr(k) + 1, :) - normalized_activations(k,:);  % erro de predicao
+        %erro(k)=Dtr(k)-ypred(k);  % erro de predicao
+        W=W+alfa*Xtr(k,:)'*erro(k,:); % Atualizacao do vetor de pesos
         acc_erro_quad=acc_erro_quad+0.5*erro(k)*erro(k);
     end
     erro_medio_epoca(t)=acc_erro_quad/Ntr;
 end
-
+elapsed_time = toc ()
 figure; plot(erro_medio_epoca);
 title('Curva de Aprendizagem');
 xlabel('Epoca de treinamento');
@@ -84,13 +118,23 @@ ylabel('Erro quadratico medio por epoca');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Passo 4: Determinar predicoes da classe dos vetores de teste %%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Ypred=W'*Xts;        % Saida como numeros reais
-Ypred_q=1./(1 + exp(-Ypred));  % Saida quantizada para +1 ou -1 usando uma sigmoide.
+Ypred=round(Xts*W);        % Saida como numeros reais
+num_test_data = size(Ypred)(1);
+
+%Mapeando os maiores valores de cada linha pra label correspondente
+for i=1:num_test_data,
+   [valor posicao] = max(Ypred(i, :));
+   Ypred_q(i) = posicao - 1;
+end
+
+
+%Ypred_q=Ypred;  % Saida quantizada para 0 ou 1 usando uma sigmoide.
+%Ypred_q=1./(1 + exp(-Ypred));  % Saida quantizada para 0 ou 1 usando uma sigmoide.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Passo 5: Determinar as taxas de acerto/erro %%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Resultados=[Dts' Ypred_q'];          % Saida desejada e predita lado-a-lado
+Resultados=[Dts Ypred_q'];          % Saida desejada e predita lado-a-lado
 Erros=Resultados(:,1)-Resultados(:,2);  % Coluna 1 - Coluna 2
 
 Nerros_pos=length(find(Erros>0))
